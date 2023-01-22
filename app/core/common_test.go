dbd903934241a57afee21dcf85adf88445c8bc9a
Dmitry Soloma <flyingmeatsoul@gmail.com>
2023-01-22 19:33:20 +0300

    Update 3

--- app/core/common_test.go
+++ app/core/common_test.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	amqp "github.com/rabbitmq/amqp091-go"
 	"testing"
 	"tutorial/app/base"
 )
@@ -17,4 +18,15 @@ func TestDatabaseVersion(t *testing.T) {
 		t.Fatal(err)
 	}
 	base.App.SuccessMessage("Postgres version is: " + version)
+}
+
+func TestRabbitMQ(t *testing.T) {
+	var message = []byte("some data")
+	p := amqp.Publishing{Body: message}
+	broker := base.App.GetRabbit()
+	e := broker.Publish(p, "tutorial.test", "local")
+	if e != nil {
+		t.Fatal(e)
+	}
+	base.App.SuccessMessage("Publish message into queue ")
 }

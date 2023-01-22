4c1f471cd05d072323cdd857996c6fa9df875f2d
Dmitry Soloma <flyingmeatsoul@gmail.com>
2023-01-22 18:48:58 +0300

    Connect to postgres

--- app/core/common_test.go
+++ app/core/common_test.go
@@ -1,10 +1,20 @@
 package core
 
 import (
-	"tutorial/app/base"
 	"testing"
+	"tutorial/app/base"
 )
 
 func TestCommon(t *testing.T) {
 	base.App.SuccessMessage("Tests are work's fine")
+}
+
+func TestDatabaseVersion(t *testing.T) {
+	var version string
+	db := base.App.GetDB()
+	err := db.QueryRow("SELECT VERSION();").Scan(&version)
+	if err != nil {
+		t.Fatal(err)
+	}
+	base.App.SuccessMessage("Postgres version is: " + version)
 }

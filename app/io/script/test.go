7212eea2d52dfb37ea9c1956a373425935ea8580
Dmitry Soloma <flyingmeatsoul@gmail.com>
2023-01-26 20:53:00 +0300

    Update 7

--- app/io/script/test.go
+++ app/io/script/test.go
@@ -9,5 +9,10 @@ import (
 func init() {
 	base.App.GetScripts()["test"] = func(args gocli.Arguments) {
 		base.App.GetLogger().Infoln("cron is works, ENV =", base.App.GetENV())
+
+		num := args["num"].GetInt()
+		if num != 0 {
+			base.App.GetLogger().Infoln("provided number is:", num)
+		}
 	}
 }

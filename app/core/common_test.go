72c8860964b2d08aa6286b288fb5ad670cffaa90
Dmitry Soloma <flyingmeatsoul@gmail.com>
2023-03-04 00:23:05 +0300

    logger test

--- app/core/common_test.go
+++ app/core/common_test.go
@@ -1,6 +1,9 @@
 package core
 
 import (
+	"context"
+	"github.com/dimonrus/gocli"
+	"github.com/dimonrus/gohelp"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"testing"
 	"tutorial/app/base"
@@ -29,4 +32,25 @@ func TestRabbitMQ(t *testing.T) {
 		t.Fatal(e)
 	}
 	base.App.SuccessMessage("Publish message into queue ")
+}
+
+func TestContextLoggerOutput(t *testing.T) {
+	t.Run("info", func(t *testing.T) {
+		config := gocli.LoggerConfig{Level: gocli.LogLevelDebug}
+		logger := gocli.NewLogger(config)
+		logger.Infoln("Some info")
+	})
+	t.Run("logger_forma", func(t *testing.T) {
+		config := gocli.LoggerConfig{
+			Level:  gocli.LogLevelErr,
+			Format: gocli.LoggerFormat{"application": "app: %s ", "requestId": "rid: %s"},
+		}
+		ctx := context.WithValue(context.Background(), "application", "tutorial")
+		ctx = context.WithValue(ctx, "requestId", gohelp.NewUUID())
+		logger := gocli.NewLogger(config)
+		// ignored according level
+		logger.Info("ignored")
+		// output with info from context
+		logger.Errorln(ctx, "Error happened")
+	})
 }
